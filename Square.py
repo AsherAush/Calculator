57850c636276bcdfba637395bdaa157c52dc0b09
asher zeev aush <A0583251203@gmail.com>
2025-06-25 09:34:22 +0300

    class Square

--- Square.py
+++ Square.py
@@ -3,9 +3,11 @@ class Square(Shape):
     def __init__(self, side_length):
         self.side_length = side_length
 
-        def get_area(self):
-            return self.side_length ** 2
+    def get_area(self):
+        return self.side_length ** 2
+
     def get_perimeter(self):
         return 4 * self.side_length
+
     def __str__(self):
         return f"Square: side_length={self.side_length}, area={self.get_area()}, perimeter={self.get_perimeter()}"
